5bdc87cc6833b192294043caf642c295dfa864dc
liwenlong <3509125291@qq.com>
2021-07-31 23:51:37 +0800

    返回json字符串

--- src/main/java/com/ljw/food_order_server/controller/StationController.java
+++ src/main/java/com/ljw/food_order_server/controller/StationController.java
@@ -2,9 +2,11 @@ package com.ljw.food_order_server.controller;
 
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ljw.food_order_server.entity.Station;
 import com.ljw.food_order_server.service.StationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -38,10 +40,19 @@ public class StationController {
         return s;
     }
 
-//    @RequestMapping("/location")
-//    public String () {
-//        return "f";
-//    }
+    /**
+     * 根据位置查询共享工位
+     * @param location 共享工位位置
+     * @return
+     */
+    @RequestMapping("/location/{location}")
+    public String listByLocation(@PathVariable String location) {
+        QueryWrapper<Station> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("location",location);
+        List<Station> list = stationService.list(queryWrapper);
+        String s = JSON.toJSONString(list);
+        return s;
+    }
 
 }
 
--- src/main/java/com/ljw/food_order_server/controller/UserController.java
+++ src/main/java/com/ljw/food_order_server/controller/UserController.java
@@ -1,9 +1,19 @@
 package com.ljw.food_order_server.controller;
 
 
+import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.mapper.Mapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.ljw.food_order_server.entity.User;
+import com.ljw.food_order_server.mapper.UserMapper;
+import com.ljw.food_order_server.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
@@ -13,9 +23,24 @@ import org.springframework.stereotype.Controller;
  * @author ljw
  * @since 2021-07-31
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 查询员工预定的共享工位信息
+     * @param userId 员工编号
+     * @return
+     */
+    @RequestMapping("/getStation/{userId}")
+    public String getStation(@PathVariable String userId){
+        QueryWrapper<User> query = Wrappers.query();
+        query.eq("user_id",userId);
+        return JSON.toJSONString(userService.list(query));
+    }
 
 }
 
